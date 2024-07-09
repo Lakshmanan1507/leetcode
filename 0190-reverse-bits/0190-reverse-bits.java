047768b08970477d2b317ec9f0b74ad986521471
lakshan c <149923410+Lakshmanan1507@users.noreply.github.com>
2024-07-09 23:17:36 +0530

    Time: 0 ms (100%), Space: 41.8 MB (32.31%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = (result << 1) | (n & 1);
+            n >>= 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int num = 432611016;
+        int reversed = solution.reverseBits(num);
+        System.out.println("Reversed bits: " + reversed);
+    }
+}
